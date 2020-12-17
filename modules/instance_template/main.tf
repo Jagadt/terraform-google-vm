d2aff8e5b981373748bee1e4f38379d57b1e520f
Jagadt <jagadt@hotmail.com>
2020-12-17 20:53:18 +0000

    Add files via upload

---
+++ modules/instance_template/main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+
+  project = var.project_id
+  region  = var.region
+  version = "~> 3.0"
+}
+
+resource "google_compute_address" "ip_address" {
+  name = "external-ip"
+}
+
+locals {
+  access_config = {
+    nat_ip       = google_compute_address.ip_address.address
+    network_tier = "PREMIUM"
+  }
+}
+
+module "instance_template" {
+  source          = "../../../modules/instance_template"
+  project_id      = var.project_id
+  subnetwork      = var.subnetwork
+  service_account = var.service_account
+  name_prefix     = "simple"
+  tags            = var.tags
+  labels          = var.labels
+  access_config   = [local.access_config]
+}
