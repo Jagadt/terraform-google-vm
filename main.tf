7dd3b6ce782a0b5d150a3c93b14f2aa692e2c39f
Jagadt <jagadt@hotmail.com>
2020-12-17 21:13:59 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,19 @@
-provider "google" {
-
-  version = "~> 3.0"
+resource "google_compute_instance" "default" {
+  region            = var.region
+  subnetwork        = var.subnetwork
+  num_instances     = var.num_instances
+  hostname          = "single-vm-instance"
+  instance_template = module.instance_template.self_link
+  access_config = [{
+    nat_ip       = var.nat_ip
+    network_tier = var.network_tier
+  }, ]
 }
-
+  
 module "instance_template" {
   source          = "./modules/instance_template"
   region          = var.region
   project_id      = var.project_id
   subnetwork      = var.subnetwork
   service_account = var.service_account
-}
-
-module "compute_instance" {
-  source            = "./modules/compute_instance"
-  region            = var.region
-  subnetwork        = var.subnetwork
-  num_instances     = var.num_instances
-  hostname          = "instance-simple"
-  instance_template = module.instance_template.self_link
-  access_config = [{
-    nat_ip       = var.nat_ip
-    network_tier = var.network_tier
-  }, ]
 }
