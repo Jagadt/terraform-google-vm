fce921c59fe4d5e01287afd8471e4219ccf198d0
Jagadt <jagadt@hotmail.com>
2020-12-17 21:03:43 +0000

    Update main.tf

--- modules/instance_template/main.tf
+++ modules/instance_template/main.tf
@@ -17,7 +17,7 @@ locals {
 }
 
 module "instance_template" {
-  source          = "../../../modules/instance_template"
+  source          = "./modules/instance_template"
   project_id      = var.project_id
   subnetwork      = var.subnetwork
   service_account = var.service_account
