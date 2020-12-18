e3e9f116f9c9519589427ceee6dd9c5e0602f73d
Jagadt <jagadt@hotmail.com>
2020-12-18 18:32:11 +0000

    Update outputs.tf

--- modules/instance_template/outputs.tf
+++ modules/instance_template/outputs.tf
@@ -1,9 +1,14 @@
- output "self_link" {
- description = "Self-link to the instance template"
- value       = module.instance_template.self_link
+output "self_link" {
+  description = "Self-link of instance template"
+  value       = google_compute_instance_template.tpl.self_link
 }
 
 output "name" {
-description = "Name of the instance templates"
-value       = module.instance_template.name
+  description = "Name of instance template"
+  value       = google_compute_instance_template.tpl.name
+}
+
+output "tags" {
+  description = "Tags that will be associated with instance(s)"
+  value       = google_compute_instance_template.tpl.tags
 }
