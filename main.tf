6ddbc07a6c398015ffda0034ee2d24f02a4fdea9
Jagadt <jagadt@hotmail.com>
2020-12-18 18:57:53 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "instance_template" {
-  source          = "../../../modules/instance_template"
+  source          = "./modules/instance_template"
   region          = var.region
   project_id      = var.project_id
   subnetwork      = var.subnetwork
@@ -7,11 +7,11 @@ module "instance_template" {
 }
 
 module "compute_instance" {
-  source            = "../../../modules/compute_instance"
+  source            = "./modules/compute_instance"
   region            = var.region
   subnetwork        = var.subnetwork
   num_instances     = var.num_instances
-  hostname          = "instance-simple"
+  hostname          = "instancevm2"
   instance_template = module.instance_template.self_link
   access_config = [{
     nat_ip       = var.nat_ip
