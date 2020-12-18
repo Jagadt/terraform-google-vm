113919cdc55c3dfc9f4008446e00e024de10968c
Jagadt <jagadt@hotmail.com>
2020-12-18 18:40:14 +0000

    Update outputs.tf

--- modules/compute_instance/outputs.tf
+++ modules/compute_instance/outputs.tf
@@ -1,9 +1,10 @@
-output "vnet_subnets" {
-  description = "The ids of subnets created inside the new vNet"
-  value       = "${azurerm_subnet.subnet.0.id}"
+output "instances_self_links" {
+  description = "List of self-links for compute instances"
+  value       = google_compute_instance_from_template.compute_instance.*.self_link
 }
 
-output "vnet_subnet_names" {
-  description = "The ids of subnets created inside the new vNet"
-  value       = "${flatten(concat(azurerm_subnet.subnet.*.name, azurerm_subnet.subnet-endpoint.*.name))}"
+output "available_zones" {
+  description = "List of available zones in region"
+  value       = data.google_compute_zones.available.names
 }
+
