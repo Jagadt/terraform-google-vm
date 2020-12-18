3d478d30e4db16a5e58407d26e746ec873cece01
Jagadt <jagadt@hotmail.com>
2020-12-18 18:59:30 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,14 +1,14 @@
 provider "google" {
   version     = "~> 3.0"
-  credentials = file(var.credentials)
-  region      = var.region
-  project     = var.project
+#  credentials = file(var.credentials)
+ # region      = var.region
+  #project     = var.project
 }
 
 provider "google-beta" {
   version     = "~> 3.0"
-  credentials = file(var.credentials)
+  #credentials = file(var.credentials)
 
-  region  = var.region
-  project = var.project
+  #region  = var.region
+  #project = var.project
 }
